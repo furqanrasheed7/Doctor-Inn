b63d38afe1d4a0ba595e630f3f10d5874b1ed39a
furqanrasheed7 <furqanrasheed927@gmail.com>
2024-02-23 01:19:36 +0500

    First Commit

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import LoginScreen from "./src/login/login.js";
+
+export default function App() {
+  return <LoginScreen />;
+}
---
+++ src/login/style.js
@@ -0,0 +1,59 @@
+const React = require("react-native");
+
+const { StyleSheet } = React;
+
+const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+    alignItems: "center"
+  },
+  loginScreenContainer: {
+    flex: 1,
+  },
+  logoText: {
+    fontSize: 40,
+    fontWeight: "800",
+    marginTop: 150,
+    marginBottom: 30,
+    textAlign: "center",
+    color: "#be2f47",
+  },
+
+    logoColor: {
+    fontSize: 40,
+    fontWeight: "800",
+    marginBottom: 30,
+    textAlign: "center",
+    color: "#337b91"
+  },
+
+  loginFormView: {
+    flex: 1,
+  },
+  loginFormTextInput: {
+    height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#eaeaea",
+    backgroundColor: "#fafafa",
+    paddingLeft: 10,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  loginButton: {
+    backgroundColor: "#3897f1",
+    borderRadius: 5,
+    height: 45,
+    marginTop: 10,
+    width: 350,
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  fbLoginButton: {
+    height: 45,
+    marginTop: 10,
+    backgroundColor: 'transparent',
+  },
+});
+export default styles;
